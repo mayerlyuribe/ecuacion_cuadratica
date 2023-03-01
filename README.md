6ba9a62940cc8a064eba384f21111cf447a3beb8
mayerlyuribe <mayerlyuribe@coleguioguanenta.edu.co>
2023-03-01 07:15:21 -0500

    se agrego el diagrama d flujo

--- README.md
+++ README.md
@@ -1,2 +1,5 @@
 # ecuacion_cuadratica
 programa para calcular ecuaciones cuadráticas, y determinar su discriminante 
+
+# diseño
+![diagrama de flujo](diagrama.png "Diagrama de flujo")
---
+++ ecuacion_cuadratica.py
@@ -0,0 +1,15 @@
+#ecuacion_cuadratica
+
+print("-------------------------------------")
+print("-- resolver la ecuacion cuadratica --")
+print("-------------------------------------")
+
+#input
+
+a= int(input)("digite el valor de el coeficiente a: ")
+b= int(input)("digite el valor de el coeficiente b: ")
+c= int(input)("digite el valor de el coeficiente c: ")
+
+#processing
+
+d=b**2-4*a*c
